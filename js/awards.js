6163698a32c2be20cae55436a623f9d4776fdb29
nero <zhanglei-js@secoo.com>
2020-01-06 14:29:19 +0800

    MODIFY AWARDS

--- js/awards.js
+++ js/awards.js
@@ -3,61 +3,61 @@ var awards = [
         level : '慰问奖',
         name : '回家车票',
         quantity : '8',
-        image : 'photo/awards/huijiachepiao.png'
+        image : 'photo/awards/huijiachepiao.jpg'
     },
     {
         level : '六等奖',
         name : '体检专项筛查',
         quantity : '1',
-        image : 'photo/awards/tijianshaicha.png'
+        image : 'photo/awards/zhuanxiangtijianjijin.jpg'
     },
     {
         level : '六等奖',
         name : '富士拍立得',
         quantity : '1',
-        image : 'photo/awards/fushipailide.png'
+        image : 'photo/awards/fushipailide.jpeg'
     },
     {
         level : '六等奖',
         name : '温暖基金（羽绒服、电热毯等）',
         quantity : '1',
-        image : 'photo/awards/wennuanjijin.png'
+        image : 'photo/awards/wennuanjijin.jpg'
     },
     {
         level : '六等奖',
         name : '美丽基金',
         quantity : '1',
-        image : 'photo/awards/meilijijin.jpeg'
+        image : 'photo/awards/meilijijn.jpg'
     },
     {
         level : '六等奖',
         name : '零食基金',
         quantity : '1',
-        image : 'photo/awards/lingshijijin.jpeg'
+        image : 'photo/awards/meishijijn.jpg'
     },
     {
         level : '六等奖',
         name : 'kindle',
         quantity : '1',
-        image : 'photo/awards/kindle.png'
+        image : 'photo/awards/kindle.jpg'
     },
     {
         level : '六等奖',
         name : '胶囊咖啡机',
         quantity : '1',
-        image : 'photo/awards/jiaonangkafeiji.png'
+        image : 'photo/awards/Nespressojiaonangkafeiji.jpg'
     },
     {
         level : '六等奖',
         name : '祖马龙香薰',
         quantity : '1',
-        image : 'photo/awards/zumalongxiangxun.png'
+        image : 'photo/awards/zumalongxiangxun.jpg'
     },
     {
         level : '六等奖',
         name : '小黄人键盘',
         quantity : '2',
-        image : 'photo/awards/xiaohuangrenjianpan.png'
+        image : 'photo/awards/LOFREE.jpg'
     },
     {
         level : '五等奖',
@@ -69,104 +69,104 @@ var awards = [
         level : '五等奖',
         name : '迪士尼门票基金',
         quantity : '2',
-        image : 'photo/awards/dishinimenpiao.png'
+        image : 'photo/awards/dishinimenpiaojijin.jpg'
     },
     {
         level : '五等奖',
         name : '全网视频会员1年（B站、优酷、腾讯、爱奇艺、百度网盘）',
         quantity : '1',
-        image : 'photo/awards/shipinwangzhanhuiyuan.jpeg'
+        image : 'photo/awards/quanwanghuiyuanjinjin.jpg'
     },
     {
         level : '五等奖',
         name : '乐高（哈利波特系列）',
         quantity : '2',
-        image : 'photo/awards/legao harry potter.png'
+        image : 'photo/awards/LEGO.jpg'
     },
     {
         level : '五等奖',
         name : '按摩基金',
         quantity : '1',
-        image : 'photo/awards/dabaojianjijin.jpeg'
+        image : 'photo/awards/anmojijin.jpg'
     },
     {
         level : '五等奖',
         name : '科沃兹扫地机器人',
         quantity : '2',
-        image : 'photo/awards/kewozisaodijiqiren.png'
+        image : 'photo/awards/Ecovacssaodijiqiren.jpg'
     },
     {
         level : '四等奖',
         name : '2000元滴滴打车充值',
         quantity : '1',
-        image : 'photo/awards/didichongzhika.jpeg'
+        image : 'photo/awards/dididachejijin.jpeg'
     },
     {
         level : '四等奖',
         name : '哈曼卡蓝牙智能音箱',
         quantity : '1',
-        image : 'photo/awards/hamankalanyayinxiang.png'
+        image : 'photo/awards/hamankalanyayinxiang.jpg'
     },
     {
         level : '四等奖',
         name : '索尼降噪耳机',
         quantity : '1',
-        image : 'photo/awards/suonierji.png'
+        image : 'photo/awards/SONY.jpg'
     },
     {
         level : '四等奖',
         name : '欧乐B电动牙刷',
         quantity : '1',
-        image : 'photo/awards/diandongyashua.png'
+        image : 'photo/awards/Oral-B.jpg'
     },
     {
         level : '三等奖',
         name : 'Switch',
         quantity : '1',
-        image : 'photo/awards/Switch.png'
+        image : 'photo/awards/Switch.jpg'
     },
     {
         level : '三等奖',
         name : 'Air Pods Pro',
         quantity : '2',
-        image : 'photo/awards/Air Pods Pro.png'
+        image : 'photo/awards/AppleAirPodsPro.jpg'
     },
     {
         level : '三等奖',
         name : '戴森吹风机',
         quantity : '1',
-        image : 'photo/awards/daisenchuifengji.png'
+        image : 'photo/awards/Dyson Supersonic.jpg'
     },
     {
         level : '三等奖',
         name : 'iWatch',
         quantity : '1',
-        image : 'photo/awards/iwatch.png'
+        image : 'photo/awards/Apple Watch.jpg'
     },
     {
         level : '三等奖',
         name : '家庭投影',
         quantity : '1',
-        image : 'photo/awards/jiatingtouying.png'
+        image : 'photo/awards/xiaomitouyingyi.jpg'
     },
     {
         level : '二等奖',
         name : 'iPad pro64G 11寸',
         quantity : '1',
-        image : 'photo/awards/iPadpro.png'
+        image : 'photo/awards/iPad Pro.jpg'
     },
     {
         level : '二等奖',
         name : '理光照相机',
         quantity : '1',
-        image : 'photo/awards/liguangzhaoxiangji.jpeg'
+        image : 'photo/awards/liguangxiangji.jpg'
     },
     {
 
         level : '一等奖',
         name : 'iPhone 11 pro 64G ',
         quantity : '1',
-        image : 'photo/awards/iPhone11pro.png'
+        image : 'photo/awards/iPhone11Pro.jpg'
     },
     {
 
